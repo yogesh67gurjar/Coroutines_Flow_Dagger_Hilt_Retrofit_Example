e746383590076407772f06fd66a0337dcebec7a3
dev_yogesh <yogesh67gurjar@gmail.com>
2024-02-25 15:45:37 +0530

    init

---
+++ app/src/main/java/com/yogesh/coroutines_flow_dagger_hilt_retrofit_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yogesh.coroutines_flow_dagger_hilt_retrofit_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
